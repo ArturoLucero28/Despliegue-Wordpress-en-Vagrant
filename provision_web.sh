d5203189ebc58f1716d98a751c82f7413c2ee441
ArturoLucero28 <146435794+ArturoLucero28@users.noreply.github.com>
2023-12-13 23:38:43 +0100

    Update provision_web.sh

--- provision_web.sh
+++ provision_web.sh
@@ -1,6 +1,10 @@
-
 sudo apt-get update
-sudo apt-get install -y nginx php-fpm
+sudo apt-get upgrade
+sudo apt-get install -y apache2 php libapache2-mod-php
+sudo apt-get install mariadb-client
+
+
+sudo mkdir -p /shared_folder
 
 
 
