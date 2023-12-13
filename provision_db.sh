2c1009f4f5fcb5b7ffa8df79be12538ea06f60a8
ArturoLucero28 <146435794+ArturoLucero28@users.noreply.github.com>
2023-12-13 23:37:46 +0100

    Update provision_db.sh

--- provision_db.sh
+++ provision_db.sh
@@ -1,12 +1,18 @@
+#!/bin/bash
 
+# Actualizar la lista de paquetes e instalar software necesario
 sudo apt-get update
 sudo apt-get install -y mariadb-server
 
+# Configurar MariaDB para permitir conexiones remotas 
+sudo sed -i 's/bind-address\s*=\s*127.0.0.1/bind-address = 0.0.0.0/' /etc/mysql/mariadb.conf.d/50-server.cnf
 
-sudo mysql -u root -p -e "CREATE DATABASE wordpress_db;"
-sudo mysql -u root -p -e "CREATE USER 'wordpress_user'@'%' IDENTIFIED BY '1234';"
-sudo mysql -u root -p -e "GRANT ALL PRIVILEGES ON wordpress_db.* TO 'wordpress_user'@'%';"
-sudo mysql -u root -p -e "FLUSH PRIVILEGES;"
-
-
+# Reiniciar el servicio MariaDB para aplicar la configuración
 sudo systemctl restart mariadb
+
+# Crear una base de datos y un usuario de ejemplo
+sudo mysql -e "CREATE DATABASE IF NOT EXISTS mydatabase;"
+sudo mysql -e "CREATE USER 'arturo'@'%' IDENTIFIED BY '1234';"
+sudo mysql -e "GRANT ALL PRIVILEGES ON mydatabase.* TO 'arturo'@'%';"
+sudo mysql -e "FLUSH PRIVILEGES;"
+
