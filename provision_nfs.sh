44c6faf7c5c55681c9fa4889db3569eee6d82a8a
ArturoLucero28 <146435794+ArturoLucero28@users.noreply.github.com>
2023-12-13 23:38:18 +0100

    Update provision_nfs.sh

--- provision_nfs.sh
+++ provision_nfs.sh
@@ -1,5 +1,18 @@
 
+# Actualizar la lista de paquetes e instalar software necesario
 sudo apt-get update
-sudo apt-get install -y nfs-kernel-server php-fpm
+sudo apt-get upgrade
+sudo apt-get install -y nfs-kernel-server
+
+# Crear directorios compartidos
+sudo mkdir -p /shared_folder
+sudo chown nobody:nogroup /shared_folder
+sudo chmod 777 /shared_folder
+
+# Configurar el archivo /etc/exports para exportar el directorio
+echo "/shared_folder *(rw,sync,no_subtree_check,no_root_squash)" | sudo tee -a /etc/exports
+
+# Reiniciar el servicio NFS para aplicar la configuración
+sudo systemctl restart nfs-kernel-server
 
 
